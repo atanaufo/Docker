28fe4946f684af7bca72d992b90cedd8e8a99e06
Atanaufo Ramalho <atanaufo.ramalho@gmail.com>
2023-08-27 14:42:45 -0300

    Atualização com a criação para Sql Server 2019

---
+++ myimage/sqlserver/init-arquivo.sh
@@ -0,0 +1,2 @@
+/opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P Sqlserver2023! -d master -i /tmp/setup.sql
+/opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P Sqlserver2023! -d master -i /tmp/insercao.sql
